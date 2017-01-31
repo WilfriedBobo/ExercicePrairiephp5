8364e509364b9bdda768d4d36b1ed370e77f958a
wilfriedbobo <wilfried.nguessan.bobo@gmail.com>
2017-01-31 09:17:35 +0100

    exercice5

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$ligne1="bada";
+$ligne2="boo";
+$ligne3="le chachacha n'aime pas les chats sourd qui font $ligne2 comme des chars en catétaire sur des minerves et entonnent les chants sur le $ligne1 ass Chuck Norris qui hurle $ligne1$ligne2";
+echo $ligne3;
+ ?>
